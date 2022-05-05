ec51dcb4a9da7b8e8e83537f0e3c910371098581
fullStackImperator <stevanv@gmx.de>
2022-05-05 21:23:22 +0200

    added reverse relationship to sales

--- src/sales/models.py
+++ src/sales/models.py
@@ -20,6 +20,10 @@ class Position(models.Model):
         self.price = self.product.price * self.quantity
         return super().save(*args, **kwargs)
 
+    def get_sales_id(self):
+        sale_obj = self.sale_set.first()    # sale reverse relationship!!!
+        return sale_obj.id
+
     def __str__(self):
         return f"id: {self.id}, product: {self.product.name}, quantity: {self.quantity}"
 
